bc9e3f3d23ff5345dc82a7e677e81b332de4286f
Chris Menz <chrismenz@Chriss-MacBook-Pro.local>
2022-07-26 13:03:55 -0400

    cd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          PHE React Frontend
         </a>
       </header>
     </div>
