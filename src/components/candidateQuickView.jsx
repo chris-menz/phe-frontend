9b56d033c2aa7e472e4489e67b3706cad87631aa
Chris Menz <chrismenz@chriss-mbp.lan>
2022-07-26 16:21:28 -0400

    basic component

---
+++ src/components/candidateQuickView.jsx
@@ -0,0 +1,9 @@
+export default function CandidateQuickView () {
+    return (
+        <main>
+            <div>
+                Candidate Quick View
+            </div>
+        </main>
+    )
+}
--- src/routes/employerView.jsx
+++ src/routes/employerView.jsx
@@ -1,7 +1,10 @@
+import CandidateQuickView from "../components/candidateQuickView";
+
 export default function EmployerView(){
     return(
         <main>
            <h1>Employer View</h1> 
+           <CandidateQuickView></CandidateQuickView>
         </main>
         
     )
