34669ce10792a2610ccc4148ed8d6528d32b7b11
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-07-26 19:59:50 -0400

    Update candidateView.jsx

--- src/routes/candidateView.jsx
+++ src/routes/candidateView.jsx
@@ -3,138 +3,64 @@ export default function CandidateView(){
         <main>
            <h1>Candidate View</h1> 
         
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <div class="card user-card">
-                <div class="card-header">
-                    <h5>Profile</h5>
-                </div>
-                <div class="card-block">
-                    <div class="user-image">
-                        <img src="jeff_winner.jpg" class="img-radius" alt="User-Profile-Image">
-                    </div>
-                    <h6 class="f-w-600 m-t-25 m-b-10">Jeffrey Bezoos</h6>
-                    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-                    <hr>
-                    <p class="text-muted m-t-15">Activity Level: 87%</p>
-                    <ul class="list-unstyled activity-leval">
-                        <li class="active"></li>
-                        <li class="active"></li>
-                        <li class="active"></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                    <div class="bg-c-blue counter-block m-t-10 p-20">
-                        <div class="row">
-                            <div class="col-4">
-                                <i class="fa fa-comment"></i>
-                                <p>1256</p>
-                            </div>
-                            <div class="col-4">
-                                <i class="fa fa-user"></i>
-                                <p>8562</p>
-                            </div>
-                            <div class="col-4">
-                                <i class="fa fa-suitcase"></i>
-                                <p>189</p>
-                            </div>
-                        </div>
-                    </div>
-                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                </div>
-            </div>
-        </div>
-        
-        <div class="col-md-4">
-            <div class="card user-card">
-                <div class="card-header">
-                    <h5>Profile</h5>
-                </div>
-                <div class="card-block">
-                    <div class="user-image">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-radius" alt="User-Profile-Image">
-                    </div>
-                    <h6 class="f-w-600 m-t-25 m-b-10">John Doe</h6>
-                    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-                    <hr>
-                    <p class="text-muted m-t-15">Activity Level: 87%</p>
-                    <ul class="list-unstyled activity-leval">
-                        <li class="active"></li>
-                        <li class="active"></li>
-                        <li class="active"></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                    <div class="bg-c-green counter-block m-t-10 p-20">
-                        <div class="row">
-                            <div class="col-4">
-                                <i class="fa fa-comment"></i>
-                                <p>1256</p>
-                            </div>
-                            <div class="col-4">
-                                <i class="fa fa-user"></i>
-                                <p>8562</p>
-                            </div>
-                            <div class="col-4">
-                                <i class="fa fa-suitcase"></i>
-                                <p>189</p>
-                            </div>
-                        </div>
-                    </div>
-                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    
-                </div>
-            </div>
-        </div>
-        
-        <div class="col-md-4">
-            <div class="card user-card">
-                <div class="card-header">
-                    <h5>Profile</h5>
-                </div>
-                <div class="card-block">
-                    <div class="user-image">
-                        <img src="blank_profile.jpg" class="img-radius" alt="User-Profile-Image">
-                    </div>
-                    <h6 class="f-w-600 m-t-25 m-b-10">Alessa Robert</h6>
-                    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-                    <hr>
-                    <p class="text-muted m-t-15">Activity Level: 87%</p>
-                    <ul class="list-unstyled activity-leval">
-                        <li class="active"></li>
-                        <li class="active"></li>
-                        <li class="active"></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                    <div class="bg-c-yellow counter-block m-t-10 p-20">
-                        <div class="row">
-                            <div class="col-4">
-                                <i class="fa fa-comment"></i>
-                                <p>1256</p>
-                            </div>
-                            <div class="col-4">
-                                <i class="fa fa-user"></i>
-                                <p>8562</p>
-                            </div>
-                            <div class="col-4">
-                                <i class="fa fa-suitcase"></i>
-                                <p>189</p>
-                            </div>
-                        </div>
-                    </div>
-                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    
+		<body style="margin-top: 50px;">
 
-                </div>
-            </div>
-        </div>
-	</div>
-</div>
-	</body>
-        </main>
+			<div class="container">
+			    <div class="row">
+				<div class="col-md-4">
+				    <div class="card user-card">
+
+					<div class="card-block">
+					    <div class="user-image">
+						<img src="jeff_winner.jpg" class="img-radius" alt="User-Profile-Image">
+					    </div>
+					    <h3 class="f-w-600 m-t-25 m-b-10">Jeffrey Bezoos</h3>
+					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
+					    <hr>
+
+
+					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+					</div>
+				    </div>
+				</div>
+
+				<div class="col-md-4">
+				    <div class="card user-card">
+
+					<div class="card-block">
+					    <div class="user-image">
+						<img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-radius" alt="User-Profile-Image">
+					    </div>
+					    <h3 class="f-w-600 m-t-25 m-b-10">Yo Mama</h3>
+					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
+					    <hr>
+
+
+					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+					</div>
+				    </div>
+				</div>
+
+				<div class="col-md-4">
+				    <div class="card user-card">
+
+					<div class="card-block">
+					    <div class="user-image">
+						<img src="blank_profile.jpg" class="img-radius" alt="User-Profile-Image">
+					    </div>
+					    <h3 class="f-w-600 m-t-25 m-b-10">John Doe</h3>
+					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
+					    <hr>
+
+
+					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+					</div>
+				    </div>
+				</div>
+				</div>
+			</div>
+		</body>
+	</main>
         
     )
 }
