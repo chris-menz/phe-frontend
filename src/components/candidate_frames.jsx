c00c6e59579ec149c1b510cb4b839c2bb05d7ad6
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-07-27 10:53:43 -0400

    Update candidate_frames.jsx

--- src/components/candidate_frames.jsx
+++ src/components/candidate_frames.jsx
@@ -1,10 +1,12 @@
 import jeff_winner from '../components/jeff_winner.jpg';
+import blank_profile from '../components/blank_profile.jpg';
 
 export default function Candidate_frames () {
     return (
 	    <main>
 			
-	    
+	   <div class="container">
+	    <div class="row">
 	        <div class="col-md-4">
 	            <div class="card user-card">
 
@@ -22,6 +24,41 @@ export default function Candidate_frames () {
 	            </div>
 	        </div>
 	   
+	        <div class="col-md-4">
+	            <div class="card user-card">
+
+	                <div class="card-block">
+	                    <div class="user-image">
+	                        <img src={https://bootdey.com/img/Content/avatar/avatar6.png} class="img-radius" alt="User-Profile-Image"/>
+	                    </div>
+	                    <h3 class="f-w-600 m-t-25 m-b-10">Yo Mama</h3>
+	                    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
+	                    <hr>
+
+
+	                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+	                </div>
+	            </div>
+	        </div>
+	        
+	        <div class="col-md-4">
+	            <div class="card user-card">
+
+	                <div class="card-block">
+	                    <div class="user-image">
+	                        <img src={blank_profile} class="img-radius" alt="User-Profile-Image"/>
+	                    </div>
+	                    <h3 class="f-w-600 m-t-25 m-b-10">John Doe</h3>
+	                    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
+	                    <hr>
+
+
+	                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+	                </div>
+	            </div>
+	        </div>
+		</div>
+	</div>
 	    </main>
 	)
 }
