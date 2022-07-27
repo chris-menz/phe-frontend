0096af5021e1a805b8df5018f117f5887e570e32
Jojomomo123 <joshuam6060@gmail.com>
2022-07-26 21:27:10 -0400

    Update candidateView.jsx

--- src/routes/candidateView.jsx
+++ src/routes/candidateView.jsx
@@ -12,11 +12,11 @@ export default function CandidateView(){
 
 					<div class="card-block">
 					    <div class="user-image">
-						<img src="jeff_winner.jpg" class="img-radius" alt="User-Profile-Image">
+						<img src="jeff_winner.jpg" class="img-radius" alt="User-Profile-Image"/>
 					    </div>
 					    <h3 class="f-w-600 m-t-25 m-b-10">Jeffrey Bezoos</h3>
 					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-					    <hr>
+					    <hr/>
 
 
 					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
@@ -29,11 +29,11 @@ export default function CandidateView(){
 
 					<div class="card-block">
 					    <div class="user-image">
-						<img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-radius" alt="User-Profile-Image">
+						<img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-radius" alt="User-Profile-Image"/>
 					    </div>
 					    <h3 class="f-w-600 m-t-25 m-b-10">Yo Mama</h3>
 					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-					    <hr>
+					    <hr/>
 
 
 					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
@@ -46,11 +46,11 @@ export default function CandidateView(){
 
 					<div class="card-block">
 					    <div class="user-image">
-						<img src="blank_profile.jpg" class="img-radius" alt="User-Profile-Image">
+						<img src="blank_profile.jpg" class="img-radius" alt="User-Profile-Image"/>
 					    </div>
 					    <h3 class="f-w-600 m-t-25 m-b-10">John Doe</h3>
 					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-					    <hr>
+					    <hr/>
 
 
 					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
