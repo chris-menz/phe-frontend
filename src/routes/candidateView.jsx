1a30031539e4e5f6cd3bebad5443f8de952cb094
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-07-27 00:22:00 -0400

    Update candidateView.jsx

--- src/routes/candidateView.jsx
+++ src/routes/candidateView.jsx
@@ -1,8 +1,10 @@
+import Candidate_frames from "../components/candidate_frames";
+
 export default function CandidateView(){
     return(
         <main>
-           <h1>Candidate View</h1> 
-        
+           	<h1>Candidate View</h1> 
+        	<Candidate_frames></Candidate_frames>
 	</main>
         
     )
