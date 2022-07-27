23761f3240c2ce50e6ed30021474b85e34bd3aed
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-07-27 00:59:50 -0400

    Update candidate_frames.jsx

--- src/components/candidate_frames.jsx
+++ src/components/candidate_frames.jsx
@@ -3,61 +3,61 @@ export default function Candidate_frames () {
 	    <main>
 // 		<body style={{margin-top: 50px;}}>
  
-			<div class="container">
-			    <div class="row"> 
-				<div class="col-md-4">
-				    <div class="card user-card">
+			<div>
+			    <div> 
+				<div>
+				    <div>
  
-					<div class="card-block">
-					    <div class="user-image">
+					<div>
+					    <div>
 // 						<img src="jeff_winner.jpg" class="img-radius" alt="User-Profile-Image"/>
 					    </div>
-					    <h3 class="f-w-600 m-t-25 m-b-10">Jeffrey Bezoos</h3>
+					    <h3>Jeffrey Bezoos</h3>
 					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
 					    <hr/>
 
 
-					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+					    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
 					</div>
 				    </div>
 				</div>
 
-				<div class="col-md-4">
-				    <div class="card user-card">
+// 				<div class="col-md-4">
+// 				    <div class="card user-card">
 
-					<div class="card-block">
-					    <div class="user-image">
-// 						<img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-radius" alt="User-Profile-Image"/>
-					    </div>
-					    <h3 class="f-w-600 m-t-25 m-b-10">Yo Mama</h3>
-					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-					    <hr/>
-
-
-					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-					</div>
-				    </div>
-				</div>
-
-				<div class="col-md-4">
-				    <div class="card user-card">
-
-					<div class="card-block">
-					    <div class="user-image">
-// 						<img src="blank_profile.jpg" class="img-radius" alt="User-Profile-Image"/>
-					    </div>
-					    <h3 class="f-w-600 m-t-25 m-b-10">John Doe</h3>
-					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-					    <hr/>
+// 					<div class="card-block">
+// 					    <div class="user-image">
+// // 						<img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-radius" alt="User-Profile-Image"/>
+// 					    </div>
+// 					    <h3 class="f-w-600 m-t-25 m-b-10">Yo Mama</h3>
+// 					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
+// 					    <hr/>
 
 
-					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-					</div>
-				    </div>
-				</div>
-				</div>
-			</div>
-// 		</body>
+// 					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+// 					</div>
+// 				    </div>
+// 				</div>
+
+// 				<div class="col-md-4">
+// 				    <div class="card user-card">
+
+// 					<div class="card-block">
+// 					    <div class="user-image">
+// // 						<img src="blank_profile.jpg" class="img-radius" alt="User-Profile-Image"/>
+// 					    </div>
+// 					    <h3 class="f-w-600 m-t-25 m-b-10">John Doe</h3>
+// 					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
+// 					    <hr/>
+
+
+// 					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+// 					</div>
+// 				    </div>
+// 				</div>
+// 				</div>
+// 			</div>
+// // 		</body>
 	    </main>
 	)
 }
