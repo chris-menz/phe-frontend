5b035f44ee2d2fd5a4028a938209860e30538ad2
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-07-27 01:02:58 -0400

    Update candidate_frames.jsx

--- src/components/candidate_frames.jsx
+++ src/components/candidate_frames.jsx
@@ -1,63 +1,60 @@
 export default function Candidate_frames () {
     return (
 	    <main>
-// 		<body style={{margin-top: 50px;}}>
- 
-// 			<div>
-// 			    <div> 
-				<div>
-				    <div>
- 
-					<div>
-					    <div>
-// 						<img src="jeff_winner.jpg" class="img-radius" alt="User-Profile-Image"/>
-					    </div>
-					    <h3>Jeffrey Bezoos</h3>
-					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-					    <hr/>
+	<div class="container">
+	    <div class="row">
+	        <div class="col-md-4">
+	            <div class="card user-card">
+
+	                <div class="card-block">
+	                    <div class="user-image">
+	                        <img src="jeff_winner.jpg" class="img-radius" alt="User-Profile-Image">
+	                    </div>
+	                    <h3 class="f-w-600 m-t-25 m-b-10">Jeffrey Bezoos</h3>
+	                    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
+	                    <hr>
 
 
-					    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-					</div>
-				    </div>
-				</div>
+	                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+	                </div>
+	            </div>
+	        </div>
+	        
+	        <div class="col-md-4">
+	            <div class="card user-card">
 
-// 				<div class="col-md-4">
-// 				    <div class="card user-card">
-
-// 					<div class="card-block">
-// 					    <div class="user-image">
-// // 						<img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-radius" alt="User-Profile-Image"/>
-// 					    </div>
-// 					    <h3 class="f-w-600 m-t-25 m-b-10">Yo Mama</h3>
-// 					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-// 					    <hr/>
-
-
-// 					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-// 					</div>
-// 				    </div>
-// 				</div>
-
-// 				<div class="col-md-4">
-// 				    <div class="card user-card">
-
-// 					<div class="card-block">
-// 					    <div class="user-image">
-// // 						<img src="blank_profile.jpg" class="img-radius" alt="User-Profile-Image"/>
-// 					    </div>
-// 					    <h3 class="f-w-600 m-t-25 m-b-10">John Doe</h3>
-// 					    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-// 					    <hr/>
+	                <div class="card-block">
+	                    <div class="user-image">
+	                        <img src="https://bootdey.com/img/Content/avatar/avatar6.png" class="img-radius" alt="User-Profile-Image">
+	                    </div>
+	                    <h3 class="f-w-600 m-t-25 m-b-10">Yo Mama</h3>
+	                    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
+	                    <hr>
 
 
-// 					    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-// 					</div>
-// 				    </div>
-// 				</div>
-// 				</div>
-// 			</div>
-// // 		</body>
+	                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+	                </div>
+	            </div>
+	        </div>
+	        
+	        <div class="col-md-4">
+	            <div class="card user-card">
+
+	                <div class="card-block">
+	                    <div class="user-image">
+	                        <img src="blank_profile.jpg" class="img-radius" alt="User-Profile-Image">
+	                    </div>
+	                    <h3 class="f-w-600 m-t-25 m-b-10">John Doe</h3>
+	                    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
+	                    <hr>
+
+
+	                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+	                </div>
+	            </div>
+	        </div>
+		</div>
+	</div>
 	    </main>
 	)
 }
