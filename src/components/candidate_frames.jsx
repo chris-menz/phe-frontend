892707a16c11e5ee3efa25291b24cf77551ae778
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-07-27 10:59:34 -0400

    Update candidate_frames.jsx

--- src/components/candidate_frames.jsx
+++ src/components/candidate_frames.jsx
@@ -29,7 +29,7 @@ export default function Candidate_frames () {
 
 	                <div class="card-block">
 	                    <div class="user-image">
-	                    	<img src={https://bootdey.com/img/Content/avatar/avatar6.png} class="img-radius" alt="User-Profile-Image"/>
+	                    	<img src={blank_profile} class="img-radius" alt="User-Profile-Image"/>
 	                    </div>
 	                    <h3 class="f-w-600 m-t-25 m-b-10">Yo Mama</h3>
 	                    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
