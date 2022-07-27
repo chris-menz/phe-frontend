ffc992032bb122fba18fa56c4a5d97a7f887b2a4
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-07-27 10:41:38 -0400

    Update candidate_frames.jsx

--- src/components/candidate_frames.jsx
+++ src/components/candidate_frames.jsx
@@ -7,17 +7,18 @@ export default function Candidate_frames () {
 	            <div class="card user-card">
 
 	                <div class="card-block">
-
+	                    <div class="user-image">
+	                        <img src="jeff_winner.jpg" class="img-radius" alt="User-Profile-Image">
+	                    </div>
 	                    <h3 class="f-w-600 m-t-25 m-b-10">Jeffrey Bezoos</h3>
 	                    <p class="text-muted">Tag1 | Tag2 | Tag3</p>
-	                   
+	                    <hr>
 
 
 	                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
 	                </div>
 	            </div>
 	        </div>
-	        
 	   
 	    </main>
 	)
