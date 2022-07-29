79d6b55f45a37f9030e4a585dd01e3729eb1c93f
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-07-29 11:12:23 -0400

    Update candidate_frames.jsx

--- src/components/candidate_frames.jsx
+++ src/components/candidate_frames.jsx
@@ -55,35 +55,35 @@ export default function Candidate_frames () {
 	            </div>
 	        </Col>
 	   
-	        <Col>
-	            <div class="card user-card">
+<Col>
+	            <div class="card user-card" style={{backgroundColor: 'white'}}>
 
 	                <div class="card-block">
-	                    <div class="user-image">
-	                    	<img src={blank_profile} class="img-radius" alt="User-Profile-Image"/>
-	                    </div>
-	                    <h3 class="f-w-600 m-t-25 m-b-10">Yo Mama</h3>
-			<p class="text-muted">Age? | Gender?</p>
-	                    <hr/>
-	                    <h4>Contact</h4>
+	                	<Row>
+	                    <Col md="7" class="user-image">
+	                        <img src={jeff_winner} width='100px' class="img-radius" alt="User-Profile-Image"/>
+	                    </Col>
+	                    <Col md="5">
+	                    	<h3 class="f-w-600 m-t-25 m-b-10 h5_black" style={{color: '#022658', marginTop: '0px'}}>Jeffrey Bezoos</h3>
+	                    	<p class="text-muted">Age? | Gender?</p>
+	                    </Col>
+	                    </Row>
+				<hr/>
 	                    <Row>
-	                    
 	                    	<Col>
-	                    		<h5><b>Call:</b></h5>
-	                    		<a href="tel:+1234567890">123-456-7890</a>
+	                    		<h5 class="h5_black"><b>Location</b></h5>
+	                    		<a href="" style={{color: 'black'}}>Link this to google maps</a>
 	                    	</Col>
 	                    	<Col>
-	                    		<h5><b>Location:</b></h5>
-	                    		<a href="">Link this to google maps</a>
+	                    		<h5 class="h5_black"><b>Bio</b></h5>
+	                    		<p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
 	                    	</Col>
 	                	</Row>
 
+	                    
 	                    <hr/>
-
-	                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-	                    <hr/>
-                    	<div class="skills">
-				<h5>Skills</h5>
+                    	<div class="skills" >
+					<h5 style={{color: 'black'}}><b>Skills</b></h5>
 					<ul>
 						<li>UI / UX</li>
 						<li>Front End Development</li>
@@ -97,36 +97,37 @@ export default function Candidate_frames () {
 	                </div>
 	            </div>
 	        </Col>
+
 	        
-	        <Col>
-	            <div class="card user-card">
+<Col>
+	            <div class="card user-card" style={{backgroundColor: 'white'}}>
 
 	                <div class="card-block">
-	                    <div class="user-image">
-	                        <img src={blank_profile} class="img-radius" alt="User-Profile-Image"/>
-	                    </div>
-	                    <h3 class="f-w-600 m-t-25 m-b-10">John Doe</h3>
-			<p class="text-muted">Age? | Gender?</p>
-	                    <hr/>
-	                    <h4>Contact</h4>
+	                	<Row>
+	                    <Col md="7" class="user-image">
+	                        <img src={jeff_winner} width='100px' class="img-radius" alt="User-Profile-Image"/>
+	                    </Col>
+	                    <Col md="5">
+	                    	<h3 class="f-w-600 m-t-25 m-b-10 h5_black" style={{color: '#022658', marginTop: '0px'}}>Jeffrey Bezoos</h3>
+	                    	<p class="text-muted">Age? | Gender?</p>
+	                    </Col>
+	                    </Row>
+				<hr/>
 	                    <Row>
-	                    
 	                    	<Col>
-	                    		<h5><b>Call:</b></h5>
-	                    		<a href="tel:+1234567890">123-456-7890</a>
+	                    		<h5 class="h5_black"><b>Location</b></h5>
+	                    		<a href="" style={{color: 'black'}}>Link this to google maps</a>
 	                    	</Col>
 	                    	<Col>
-	                    		<h5><b>Location:</b></h5>
-	                    		<a href="">Link this to google maps</a>
+	                    		<h5 class="h5_black"><b>Bio</b></h5>
+	                    		<p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
 	                    	</Col>
 	                	</Row>
 
+	                    
 	                    <hr/>
-
-	                    <p class="m-t-15 text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-	                    <hr/>
-                    	<div class="skills">
-				<h5>Skills</h5>
+                    	<div class="skills" >
+					<h5 style={{color: 'black'}}><b>Skills</b></h5>
 					<ul>
 						<li>UI / UX</li>
 						<li>Front End Development</li>
