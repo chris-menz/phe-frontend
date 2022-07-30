b9d3159b3b2b0e625d1c43d7b1ebce811a26ba2d
Chris Menz <chrismenz@Chriss-MacBook-Pro.local>
2022-07-29 22:41:12 -0400

    file names

--- src/routes/candidateView.jsx
+++ src/routes/candidateView.jsx
@@ -1,10 +1,7 @@
-import Candidate_frames from "../components/candidateFrames";
-
 export default function CandidateView(){
     return(
         <main>
            	<h1>Candidate View</h1> 
-        	<Candidate_frames></Candidate_frames>
 	</main>
          
     )
