d1524fc2b89565413cc3361f594da1e7118740f4
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-08-03 16:37:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,18 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          PHE React Frontend
-        </a>
+
         <a>
           <Link to="/candidateView">Candidate View</Link>
           <Link to="/employerView">Employer View</Link>
