c674f91089bb97f3200c77d1dfafe9ab7e08c365
duck_master <duckeagle1280@gmail.com>
2022-08-03 14:46:22 -0400

    created about.jsx in main

---
+++ src/routes/about.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+        <h1>ABOUT PROJECT HOMELESS EMPLOYMENT</h1>
+            <p>We are a small team of people. Our vision is to use the power of software to enable homeless people to get jobs. To this end, we are building this website throughout the spring and summer of 2022. Many of us were students at Newton South High School.</p>
+
+            <p>To date, we are the following people:</p>
+            <ul>
+                <li>Benson Lu: leader</li>
+                <li>Zach Schwartz: leader</li>
+                <li>Joshua Guo: coder</li>
+                <li>Will Huang: coder</li>
+                <li>Phillip Amitan: coder</li>
+                <li>Chris Menz: coder</li>
+                <li>Joshua Moshes: coder</li>
+                <li>Deon Chan: coder</li>
+            </ul>
+
+            <p>THIS PAGE IS UNDER CONSTRUCTION!!!!!</p>
+        </div>
+	);
+};
+
+export default AboutPage;
