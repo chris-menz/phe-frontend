93a68874e89cb105e1dd7f569b9461760f3ea3a4
duck_master <duckeagle1280@gmail.com>
2022-08-03 14:51:46 -0400

    created faq.jsx

---
+++ src/routes/faq.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FAQPage = () => {
+    return (
+        <div>
+        <h1>FAQ</h1>
+            <p>There are no frequently asked questions.</h1>
+        </div>
+	);
+};
+
+export default FAQPage;
