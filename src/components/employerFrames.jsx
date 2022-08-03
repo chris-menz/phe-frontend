b0d6f9ce04cb80af4ac7a02c5f02d668ccf7342e
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-08-03 15:07:45 -0400

    Update employerFrames.jsx

--- src/components/employerFrames.jsx
+++ src/components/employerFrames.jsx
@@ -5,7 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-export default function Candidate_frames () {
+export default function Employer_frames () {
     return (
 	    <main>
 			
