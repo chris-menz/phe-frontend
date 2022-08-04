5635cc8b078cee4f3980f093028e10316d49ea3e
meppydc <49413486+meppydc@users.noreply.github.com>
2022-08-03 22:46:34 -0400

    switch registerer button

--- src/routes/registerView.jsx
+++ src/routes/registerView.jsx
@@ -2,19 +2,26 @@ import CandidateRegister from "../components/candidateRegister";
 import EmployerRegister from "../components/employerRegister";
 
 import Button from "react-bootstrap/Button";
+import React, {useState} from 'react';
 
 export default function RegisterView () {
+
+    const [isEmployerView, switchView] = useState(false)
+
+
     return (
         <main>
             <h1>Register View</h1>
-            <div style={{borderStyle: 'solid', borderColor: 'blue'}}>
+            <div style={{borderStyle: 'solid', borderColor: isEmployerView ? 'black' : 'red'}}>
                 <CandidateRegister></CandidateRegister>
             </div>
             <div style={{paddingTop: '20px'}}></div>
-            <div style={{borderStyle: 'solid', borderColor: 'blue'}}>
+            <div style={{borderStyle: 'solid', borderColor: isEmployerView? 'red' : 'black'}}>
             <EmployerRegister></EmployerRegister>
             </div>
-            <Button variant="primary" size="lg">Switch</Button>
+            <div>
+            <Button variant="primary" size="lg" onClick={() => switchView(!isEmployerView)}>Switch to {isEmployerView ? 'Candidate' : 'Employer'}</Button>
+            </div>
         </main>
     )
 }
