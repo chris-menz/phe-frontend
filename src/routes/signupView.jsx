602c64ae2f2062a4043b8ab876f2cf73a46c45a8
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-08-17 20:28:23 -0400

    Create signupView.jsx

---
+++ src/routes/signupView.jsx
@@ -0,0 +1,11 @@
+import EmployerFrames from "../components/signupFrames";
+export default function signupView(){
+    return(
+        <main>
+           	<h1>Sign Up</h1> 
+	    <SignupFrames></SignupFrames>
+	</main>
+          
+    )
+}
+ 
