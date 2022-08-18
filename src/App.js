8222d8238a90b99523eeb7489b9205f82c9aa838
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-08-17 20:31:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Link } from 'react-router-dom';
 import candidateView from "./routes/candidateView"
 import employeeView from "./routes/employerView"
+import signupView from "./routes/signupView"
 import about from "./routes/about"
 import faq from "./routes/faq"
 import MainPage from "./routes/mainpage"
@@ -19,6 +20,7 @@ function App() {
 	        <Link to="/about">About</Link>
 	        <Link to="/FAQ">FAQ</Link>
 	        <Link to="/MainPage">Our (intended) main page</Link>
+	<Link to="/signupView">Sign Up</Link>
         </a>
       </header>
     </div>
