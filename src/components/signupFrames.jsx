3373203f663776225dc27a569e1775661c5ed720
Jojomomo123 <64051148+Jojomomo123@users.noreply.github.com>
2022-08-17 20:28:20 -0400

    Create signupFrames.jsx

---
+++ src/components/signupFrames.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export default function Signup_frames () {
+    return (
+      <main>
+      
+      </main>
+     )
+}
