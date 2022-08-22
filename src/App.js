65900a1045d36327d82b2d7953a2bfd95c38ed7d
duck_master <duckeagle1280@gmail.com>
2022-08-22 16:10:27 -0400

    rewrite app.js

--- src/App.js
+++ src/App.js
@@ -3,37 +3,36 @@ import './App.css';
 import { Link } from 'react-router-dom';
 import candidateView from "./routes/candidateView"
 import employeeView from "./routes/employerView"
-import registerView from './routes/registerView';
 import about from "./routes/about"
 import faq from "./routes/faq"
-import MainPage from "./routes/mainpage"
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          PHE React Frontend
-        </a>
-        <a>
-          <Link to="/registerView">Register View</Link>
-          <Link to="/candidateView">Candidate View</Link>
-          <Link to="/employerView">Employer View</Link>
-	        <Link to="/about">About</Link>
-	        <Link to="/FAQ">FAQ</Link>
-	        <Link to="/MainPage">Our (intended) main page</Link>
-        </a>
-      </header>
+	<div className = "App">
+        <div className = "App-header">
+            	<h1> PROJECT HOMELESS EMPLOYMENT</h1>
+            	<p> We use software to give homeless people jobs! </p>
+	</div>
+	<div>
+            <h2>Explore</h2>
+            <p> explore our websiteeeeeeee</p>
+	    <ul>
+          	<li><Link to="/registerView">Register View</Link></li>
+          	<li><Link to="/candidateView">Candidate View</Link></li>
+          	<li><Link to="/employerView">Employer View</Link></li>
+	        <li><Link to="/about">About</Link></li>
+	        <li><Link to="/FAQ">FAQ</Link></li>
+	    </ul>
+            <div>
+                <button class="btn">Dark</button>
+            </div>
+            <script src="app.js"></script>
+            <noscript>You need to enable JavaScript to view the full site.</noscript>
+	</div>
+	<div>
+            <p>THIS PAGE IS UNDER CONSTRUCTION!!!</p>
+        </div>
     </div>
   );
 }
